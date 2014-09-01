f1caf0d2a3670dd796c02d6060dccd6f9cb14b98
alex <alex@zengers.de>
2014-09-01 22:45:46 +0200

    added initial working version

--- src/check_rkhunter_logfile.sh
+++ src/check_rkhunter_logfile.sh
@@ -24,38 +24,54 @@ VERSION=0.1
 # Variables
 #  -f
 logFile="/var/log/rkhunter.log"
-# -m
 
+## default warning and critical values
+# -q
 warnFileAgeMinutes="1440"
+# -r
 critFileAgeMinutes="1440"
-
+# -s
 warnSuspectFiles="1"
+# -t
 critSuspectFiles="1"
-
+# -u
 warnPossibleRootkits="1"
+# -v
 critPossibleRootkits="1"
-
+# -w
 warnSuspectApplications="1"
+# -x
 critSuspectApplications="1"
-
+# -y
 warnNumberOfWarnings="1"
+# -z
 critNumberOfWarnings="1"
+
 
 # functions
 function usage {
- echo "This script checks the output ..."
- echo ""
- echo "Usage: $SCRIPTNAME [-h] [-v] [-w warning threshold] [-c critical threshold]"
- echo "                   [ FIXME ]"
- echo ""
- echo "Options:"
- echo "-h Print this help message"
- echo "-v Print version"
- echo "-f Sets path to rkhunter logfile (default: ${logFile}"
- echo "-m Sets path to rkhunter logfile (default: ${logFile}"
- echo "-w Sets warning threshold (default: 10)"
- echo "-c Sets critical threshold (default: 60)"
- [[ $# -eq 1 ]] && exit $1 || exit $EXIT_FAILURE
+  echo "This script checks the output of a rkhunter logfile and alerts, if"
+  echo "certain limits are overstepped. Critical thresholds are"
+  echo "overtrumped by warning thresholds."
+  echo ""
+  echo "Usage: $SCRIPTNAME [-h] [-v] [-f rkhunter logfile]"
+  echo "                   [ -qrstuvwxyz thresholds (see options)]"
+  echo ""
+  echo "Options:"
+  echo "-h Print this help message"
+  echo "-v Print version"
+  echo "-f Sets path to rkhunter logfile (default: ${logFile})"
+  echo "-q warning threshold for logfile age in minutes (default: ${warnFileAgeMinutes})"
+  echo "-r critical threshold for logfile age in minutes (default: ${critFileAgeMinutes})"
+  echo "-s warning threshold for number of suspected files (default: ${warnSuspectFiles})"
+  echo "-t critical threshold for number of suspected files (default: ${critSuspectFiles})"
+  echo "-u warning threshold for number of possible rootkits (default: ${warnPossibleRootkits})"
+  echo "-v critical threshold for number of possible rootkits (default: ${critPossibleRootkits})"
+  echo "-w warning threshold for number of suspected applications (default: ${warnSuspectApplications})"
+  echo "-x critical threshold for number of suspected applications (default: ${critSuspectApplications})"
+  echo "-y warning threshold for number of warnings found in logfile (default: ${warnNumberOfWarnings})"
+  echo "-z critical threshold for number of warnings found in logfile (default: ${critNumberOfWarnings})"
+  [[ $# -eq 1 ]] && exit $1 || exit $EXIT_FAILURE
 }
 
 # Die Option -h fuer Hilfe sollte immer vorhanden sein, die Optionen
@@ -89,18 +105,88 @@ while getopts 'f:m:vh' OPTION ; do
 done
 
 # check if logfile exists
-if [[ -f "${logfile}" ]]; then
+if [[ ! -f "${logFile}" ]]; then
   echo "ERROR: logfile not found at: ${logFile}"
+  echo ""
+  usage $EXIT_ERROR
+fi
+
+# check if logfile can be read
+if [[ ! -r "${logFile}" ]]; then
+  echo "ERROR: logfile could not be read at: ${logFile}"
+  echo ""
   usage $EXIT_ERROR
 fi
 
 
 # all fine, begin actual work
 
-# grep "Suspect files:" rkhunter.log | awk '{print $NF}'
-# grep "Possible rootkits:" rkhunter.log | awk '{print $NF}'
-# grep "Suspect applications:" rkhunter.log | awk '{print $NF}'
-# find rkhunter.log -mmin +1440
-# grep "\[ Warning \]" rkhunter.log | wc -l
+# get numbers of values
+curSuspectFiles=`grep "Suspect files:" ${logFile} | awk '{print $NF}'`
+curPossibleRootkits=`grep "Possible rootkits:" ${logFile} | awk '{print \
+$NF}'`
+curSuspectApplications=`grep "Suspect applications:" ${logFile} | awk \
+'{print $NF}'`
+curNumberOfWarnings=`grep "\[ Warning \]" ${logFile} | wc -l`
 
+# check for criticals
+critOldFile=`find ${logFile} -mmin +${critFileAgeMinutes}`
+if [ ${critOldFile} ]; then
+  echo "CRITICAL: logfile at ${logFile} is older than ${critFileAgeMinutes} \
+minutes!"
+  exit ${EXIT_CRITICAL}
+fi
 
+if [ "${curSuspectFiles}" -ge "${critSuspectFiles}" ]; then
+  echo "CRITICAL: rkhunter found ${curSuspectFiles} suspected files!"
+  exit ${EXIT_CRITICAL}
+fi
+
+if [ "${curPossibleRootkits}" -ge "${critPossibleRootkits}" ]; then
+  echo "CRITICAL: rkhunter found ${curPossibleRootkits} possible rootkits!"
+  exit ${EXIT_CRITICAL}
+fi
+
+if [ "${curSuspectApplications}" -ge "${critSuspectApplications}" ]; then
+  echo "CRITICAL: rkhunter found ${curSuspectApplications} suspect \
+applications!"
+  exit ${EXIT_CRITICAL}
+fi
+
+if [ "${curNumberOfWarnings}" -ge "${critNumberOfWarnings}" ]; then
+  echo "CRITICAL: rkhunter found ${curNumberOfWarnings} suspect \
+applications!"
+  exit ${EXIT_CRITICAL}
+fi
+
+# check for warnings
+warnOldFile=`find ${logFile} -mmin +${warnFileAgeMinutes}`
+if [ ${warnOldFile} ]; then
+  echo "WARNING: logfile at ${logFile} is older than ${warnFileAgeMinutes} \
+minutes!"
+  exit ${EXIT_WARNING}
+fi
+
+if [ "${curSuspectFiles}" -ge "${warnSuspectFiles}" ]; then
+  echo "WARNING: rkhunter found ${curSuspectFiles} suspected files!"
+  exit ${EXIT_WARNING}
+fi
+
+if [ "${curPossibleRootkits}" -ge "${warnPossibleRootkits}" ]; then
+  echo "WARNING: rkhunter found ${curPossibleRootkits} possible rootkits!"
+  exit ${EXIT_WARNING}
+fi
+
+if [ "${curSuspectApplications}" -ge "${warnSuspectApplications}" ]; then
+  echo "WARNING: rkhunter found ${curSuspectApplications} suspect \
+applications!"
+  exit ${EXIT_WARNING}
+fi
+
+if [ "${curNumberOfWarnings}" -ge "${warncritNumberOfWarnings}" ]; then
+  echo "WARNING: rkhunter found ${curNumberOfWarnings} suspect \
+applications!"
+  exit ${EXIT_WARNING}
+fi
+
+exit ${EXIT_OK}
